b5165d837fa0c2979903e856e948374c6903b97d
RameenH26 <76857653+RameenH26@users.noreply.github.com>
2021-07-15 13:21:34 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.position(480, 230);
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+}
+function draw() {
+    image(video, 0 ,0, 300, 300);
+}
